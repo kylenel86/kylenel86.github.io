70de3808bdad1efebbc4bfb40b5b7d36358e59fa
kylenel86 <mcc13036@byui.edu>
2019-09-21 18:13:40 -0600

    update

--- js/code.js
+++ js/code.js
@@ -1,7 +1,7 @@
-var lastupdate = document.lastModified;
+{var lastupdate = document.lastModified;
 var lu = document.querySelector(".lastupdate");
 lu.innerHTML = lastupdate;
 
 var currentyear = new Date().getFullYear();
 var cy = document.querySelector(".currentyear");
-cy.innerHTML = currentyear
+cy.innerHTML = currentyear}

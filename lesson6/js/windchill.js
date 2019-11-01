c5820abc13650c1c671b1640c29cabfdad1f6de4
kylenel86 <mcc13036@byui.edu>
2019-10-31 21:37:30 -0600

    tr

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -15,6 +15,6 @@ function newChill(){
             /*if (temp <= 50 && speed > 3) {  
                 f = 32.74 + (0.6215 * temp)-(35.75 * Math.pow(speed,0.16)) + (0.4275 * temp * Math.pow(speed,0.16));
             }*/
-            return speed;
+            return temp;
     
     }

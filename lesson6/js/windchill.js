5b61f9c040178e3b3c70eaacfe5d156fbd661780
kylenel86 <mcc13036@byui.edu>
2019-10-31 21:39:56 -0600

    try

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -10,11 +10,11 @@ function newChill(){
     
     
         function windChill(temp, speed){
-            //var f = 'n/a';
+            var f = 'n/a';
             //var f = speed:
-            /*if (temp <= 50 && speed > 3) {  
+            if (temp <= 50 && speed > 3) {  
                 f = 32.74 + (0.6215 * temp)-(35.75 * Math.pow(speed,0.16)) + (0.4275 * temp * Math.pow(speed,0.16));
-            }*/
-            return temp;
+            }
+            return f;
     
     }

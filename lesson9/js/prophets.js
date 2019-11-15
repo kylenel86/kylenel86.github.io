8651fc3e6b149fdd3b13f3a713e0d179d64de8e2
kylenel86 <mcc13036@byui.edu>
2019-11-14 20:32:36 -0700

    try

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,16 +1,16 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL = 'https://byui-cit230.github.io/canvas-referenced/latter-day-prophets.json';
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-   //console.table(jsonObject);  // temporary checking for valid response and data parsing
- 
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
   const prophets = jsonObject['prophets'];
+  
   for (let i = 0; i < prophets.length; i++ ) {
       let card = document.createElement('section');
-      let h2 = document.createElement('h2');
+let h2 = document.createElement('h2');
 
 h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
 
@@ -18,6 +18,7 @@ card.appendChild(h2);
 
 document.querySelector('div.cards').appendChild(card);
 
-image.setAttribute('imageurl', prophets[i].imageurl);
+image.setAttribute('src', prophets[i].imageurl);
   }
-});
+  });
+

7a06361e99d619d5dcf718a0347a9b6a8f9dac3f
kylenel86 <mcc13036@byui.edu>
2019-11-14 19:17:11 -0700

    try

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -5,9 +5,9 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-   console.table(jsonObject);  // temporary checking for valid response and data parsing
+   //console.table(jsonObject);  // temporary checking for valid response and data parsing
  
-  const prophets = jsonObject['prophets'];
+  const prophets = jsonObject['cards'];
   for (let i = 0; i < prophets.length; i++ ) {
       let card = document.createElement('section');
       let h2 = document.createElement('h2');

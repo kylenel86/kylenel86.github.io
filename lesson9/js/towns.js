90d34726d96add91350631852614eb6d9b63f0ff
kylenel86 <mcc13036@byui.edu>
2019-11-17 00:04:43 -0700

    up

--- lesson9/js/towns.js
+++ lesson9/js/towns.js
@@ -15,7 +15,7 @@ fetch(requestURL)
     if(towns[i].name == "Preston" ||  towns[i].name == "Soda Springs" || towns[i].name == "Fish Haven")
         {
 
-      //let creates variables in this html tag document. the document is the web page we are referencing here in this case, the weather page index.html where we have referenced this js file
+      
 
         let card = document.createElement("section");
         let span = document.createElement("span");
@@ -27,8 +27,6 @@ fetch(requestURL)
         let photo = document.createElement("img");
     
         
-      //here we are calling out what we created in let, similiar to the idea of calling by id or class.  textContent gets the content of all elements, including <script> and <style> elements. In contrast, innerText only shows “human-readable” elements. 
-      
 
         h2.textContent = towns[i].name;
         motto.textContent = towns[i].motto;
@@ -36,10 +34,6 @@ fetch(requestURL)
         currentPopulation.textContent = 'Current Population:' + ' ' + towns[i].currentPopulation;
         averageRainfall.textContent = 'Average Rainfall:' + ' ' + towns[i].averageRainfall;
 
-//        birthplace.textContent = 'Place of Birth:' + ' ' + prophets[i].birthplace;
-
-
-        // Here we are putting a tag inside another tog, we are putting the span inside of the card and above we have called the card a section.
 
         card.appendChild(span);
         span.appendChild(h2);
@@ -50,12 +44,11 @@ fetch(requestURL)
         card.appendChild(photo);
         
 
-        //can set class id language Sets the value of an attribute on the specified element. If the attribute already exists, the value is updated; otherwise a new attribute is added with the specified name and value. To get the current value of an attribute, use getAttribute(); to remove an attribute, call removeAttribute().
 
         photo.setAttribute('src', 'images/' + towns[i].photo); 
         photo.setAttribute('alt', towns[i].name);
 
-        //I am taking all the spans I created above and putting them in .towns span
+        
         document.querySelector('span.towns').appendChild(card);
     }
   }

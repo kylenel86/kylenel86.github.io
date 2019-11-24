9275013a049f122d1066b49ee3261ffaea9a1b9f
kylenel86 <mcc13036@byui.edu>
2019-11-23 22:46:47 -0700

    js

---
+++ lesson10/js/bannerHeader.js
@@ -0,0 +1,7 @@
+function pancakebrk(){
+    var wd = new Date().getDay();
+    
+    if (wd == 5) {
+        document.getElementById("pancakes").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."
+    }
+    }
